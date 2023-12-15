cc0ec81541da52c9e7de46f35dd159cbf6211e47
Viliam <vpopove@tbdir.net>
2023-12-15 06:34:44 +0100

    Refactor day 15

--- 15/main.py
+++ 15/main.py
@@ -1,10 +1,8 @@
 with open('input.txt') as rf:
     words = rf.read().strip()
 
-# ttl = 0
 boxes = {i: [] for i in range(256)}
 for word in words.split(','):
-    # The result of running the HASH algorithm on the label indicates the correct box for that step.
     print(word)
     vl = 0
     label = word.split('-')[0].split('=')[0]
@@ -13,7 +11,6 @@ for word in words.split(','):
         vl = (vl * 17) % 256
     if '-' in word:
         boxes[vl] = [x for x in boxes[vl] if label != x.split()[0]]
-        # move any remaining lenses as far forward in the box as they can go without changing their order?
     if '=' in word:
         focal_len = word[-1]
         lens_in_box = [x for x in boxes[vl] if label == x.split()[0]]
@@ -22,10 +19,7 @@ for word in words.split(','):
             boxes[vl][indx] = f"{label} {focal_len}"
         else:
             boxes[vl].append(f"{label} {focal_len}")
-    print(boxes)
-    # ttl += vl
-# print(ttl)
-print(boxes)
+
 ttl = 0
 for box_num, values in boxes.items():
     for lens_indx, vl in enumerate(values):
